b3d796dc6a8cc9570013acb13533bb2bee4e9360
meol <dotnet.caboodle@gmail.com>
2025-01-07 21:45:15 +0000

    Working demo

--- newmulti/src/app/app.component.ts
+++ newmulti/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MultiStepFormComponent } from './components/multi-step-form/multi-step-form.component';
+import { STEP_ITEMS } from './constants/multi-step-form';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MultiStepFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'newmulti';
+
+  formContent: any;
+  formData: any;
+  activeStepIndex!: number;
+
+  ngOnInit(): void {
+    this.formContent = STEP_ITEMS;
+    this.formData = {};
+  }
+
+  onFormSubmit(formData: any): void {
+    this.formData = formData;
+
+    // post form data here
+    alert(JSON.stringify(this.formData));
+  }
 }
---
+++ newmulti/src/app/pipes/format-title.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatTitle',
+  standalone: true
+})
+export class FormatTitlePipe implements PipeTransform {
+
+  transform(title: string): string {
+    const result = title.replace(/([A-Z])/g, ' $1');
+    const finalResult = result.charAt(0)
+      .toUpperCase() + result.slice(1);
+
+    return finalResult;
+  }
+
+}
